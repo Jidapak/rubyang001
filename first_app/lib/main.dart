4103daa0cd27ab38c12facc08beb6693036d2760
kama.ji <kama.jidapa@gmail.com>
2023-09-03 17:47:14 +0700

    Page and Listview

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,13 +10,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-    
       theme: ThemeData(
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Hello Flutter'),
+      initialRoute:'/first' ,
+      routes : {
+      '/first': (context) => FirstPage(),
+      '/second': (context) => SecondPage(),
+      '/third': (context) => ThirdPage(),
+      },
     );
   }
 }
@@ -77,6 +79,10 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            SizedBox(
+              width:100.0 ,
+            child: Image.asset('images/bojung.jpg'),
+            ),
             const Text(
               'You have pushed the button this many times:',
             ),
@@ -141,4 +147,121 @@ class _MyHomePageState extends State<MyHomePage> {
       ), 
     );
   }
+}
+
+class FirstPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('First Page'),
+        actions: [
+          IconButton(onPressed:(){
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Hello ...'),
+                ),
+            );
+          }, 
+          icon: Icon(Icons.add_alert), 
+          ),
+          IconButton(onPressed:(){
+            Navigator.pushNamed(context, '/second');
+          }, 
+          icon: Icon(Icons.navigate_next),
+          ),
+        ],
+      ) ,
+      body: Center(
+        child: Image.network ('https://i.pinimg.com/564x/57/52/01/5752016f1311cf6624af3da7266cb2db.jpg'
+        ),
+      ),
+    );
+  } 
+}
+class SecondPage extends StatelessWidget{
+  final List<String> entries = ['A','B','C','D','E'];
+  final List<int> colorCodes =[600, 500,400,300,100];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Second Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.pushNamed(context, '/third');
+            },
+           icon: Icon(Icons.navigate_next),
+          ),
+        ],
+      ) ,
+      body: ListView.separated(
+        padding:const EdgeInsets.all(12.0),
+        itemBuilder: (context, index ){
+          return Container(
+            height: 150.0 ,
+            color: Colors.green[colorCodes[index]],
+            child: Center(
+              child:Text('Item ${entries[index]}'),
+               ),
+          );
+        },
+        separatorBuilder:(context, index ) => Divider() ,
+        itemCount: entries.length,
+        ),
+      );
+  }
+}
+class ThirdPage extends StatelessWidget{
+   final List<String> imagePaths=[
+  'images/babyPolar.png',
+  'images/Poling.png',
+    'images/Maru.png',
+    'images/Sushi.png',
+    'images/Polar.png',
+    'images/Fatmuji.png',
+    'images/Poli.png',
+    'images/Tonhom.png',
+    'images/Yumi.png',
+     'images/Muji.png',
+   ];
+   final List<String> entries = ['A','B','C','D','E','F','G','H','I','J'];
+  final List<int> colorCodes =[500,300,500,300,500,300,500,300,500,300,500,300];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar:AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Third Page'),
+        actions: [
+          IconButton(
+            onPressed: (){
+              Navigator.pushNamed(context, 'Flutter Demo');
+            },
+           icon: Icon(Icons.navigate_next),
+          ),
+        ],
+      ) ,
+       body: ListView.separated(
+         padding:const EdgeInsets.all(10.0),
+          itemBuilder: (context, index ){
+          return Container(
+          color: Colors.green[colorCodes[index]],
+          child : Image.asset(
+              imagePaths[index],
+              width: 150.0, // Set the desired width for each image
+              height: 150.0,
+          ),
+          );
+          },    
+        itemCount: imagePaths.length,
+        separatorBuilder: (BuildContext context, int index) {
+          return Divider(); // Add a separator between images
+        },
+        ),
+      );
+  } 
 }
